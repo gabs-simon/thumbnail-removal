33f88b667a1fa803bb9b75a7c614eec28bc88970
Gabriel Simonetti <simonettigo@gmail.com>
2018-09-26 00:13:41 -0300

    Now creates thumbnails for images

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 // Lê arquivos de configuração do dotenv.
 require('dotenv').config();
-let AWS = require('aws-sdk');
+const AWS = require('aws-sdk');
+const sharp = require('sharp');
+
+const thumbnail_sizes = [
+  {size : 'p', dimensions: '370x281'},
+  {size : 'm', dimensions: '600x464'},
+  {size : 'g', dimensions: '1440x600'}
+];
 
 let cred = new AWS.Credentials(process.env.AWS_KEY, process.env.AWS_SECRET);
 
@@ -34,6 +41,37 @@ let deleteObject = name => {
   });
 };
 
+let createThumbnails = name => {
+  let params = {
+    Bucket: process.env.AWS_BUCKET,
+    Key: name
+  };
+
+  S3.getObject(params, (err1, inputImage) => {
+    if(err1){
+      console.log("Error retrieving image from S3");
+    } else {
+      thumbnail_sizes.forEach(size => {
+        const sizes = size.dimensions.split('x');
+        sharp(inputImage.Body)
+          .resize(parseInt(sizes[0]), parseInt(sizes[1]))
+          .toBuffer()
+          .then(outputImage => {
+            let uploadParams = Object.assign({}, params);
+            uploadParams.Body = outputImage;
+            uploadParams.Key = params.Key.replace('public/images/', 'public/images/' + size.size + '_');
+
+            S3.upload(uploadParams, function(err2){
+              if(err2){
+                console.log("Error uploading image to S3");
+              }
+            });
+          })
+      });
+    }
+  })
+};
+
 /**
  * Processa um arquivo para checar se ele deve ser mantido ou removido.
  * @param file
@@ -47,6 +85,12 @@ let processSingleFile = file => {
     let dimensions = prefix.split('x'); // Checa se o nome segue o formato NUMxNUM
     if(dimensions.length === 2 && !isNaN(dimensions[0]) && !isNaN(dimensions[1])){
       deleteObject(file.Key);
+    }
+  } else {
+    const isOnImageFolder = name.indexOf('public/images/' !== -1);
+    const isImage = (name.indexOf('.jpg') !== -1 || name.indexOf('.png') !== -1 || name.indexOf('.gif') !== -1 || name.indexOf('.jpeg') !== -1);
+    if(isOnImageFolder && isImage){
+      createThumbnails(name);
     }
   }
 };
